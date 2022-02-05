093debf907002193d0efcea0713e7fd3a187b195
ismailPervez <freelanceismail001@gmail.com>
2022-02-05 11:00:15 +0300

    user can now register successfully

--- pictogram/users/views.py
+++ pictogram/users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'users/index.html')
@@ -9,8 +10,11 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             print('form saved cool')
-
+            form.save()
+            print('saved form, check admin')
+            messages.success(request, 'Registration suceessful. You can now login')
+            # redirect('')
     else:
         form = RegisterForm()
-        
+
     return render(request, 'users/register.html', {'form': form})
